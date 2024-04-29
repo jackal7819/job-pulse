c5c7df4b80702fa8c57a7ed1797c872843d6fe2a
jackal7819 <jackal3166@gmail.com>
2024-04-29 20:44:39 +0300

    Add types

---
+++ utils/types.ts
@@ -0,0 +1,41 @@
+import * as z from 'zod';
+
+export type JobType = {
+	id: string;
+	createdAt: string;
+	updateAt: string;
+	clerkId: string;
+	position: string;
+	company: string;
+	location: string;
+	status: JobStatus;
+	mode: JobMode;
+};
+
+export enum JobStatus {
+	Pending = 'pending',
+	Interview = 'interview',
+	Declined = 'declined',
+}
+
+export enum JobMode {
+	FullTime = 'full-time',
+	PartTime = 'part-time',
+	Internship = 'internship',
+}
+
+export const createAndEditJobSchema = z.object({
+	position: z
+		.string()
+		.min(3, { message: 'Position should be at least 3 characters' }),
+	company: z
+		.string()
+		.min(3, { message: 'Company should be at least 3 characters' }),
+	location: z
+		.string()
+		.min(3, { message: 'Location should be at least 3 characters' }),
+	status: z.nativeEnum(JobStatus),
+	mode: z.nativeEnum(JobMode),
+});
+
+export type CreateAndEditJobInput = z.infer<typeof createAndEditJobSchema>;
