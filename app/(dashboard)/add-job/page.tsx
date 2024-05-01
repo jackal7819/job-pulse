f9a7cfae7685b5fb0507048dc727e865d63672f6
jackal7819 <jackal3166@gmail.com>
2024-05-01 14:32:08 +0300

    Add HydrationBoundary

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,9 +1,16 @@
-import CreateJobForm from '@/components/CreateJobForm'
+import CreateJobForm from '@/components/CreateJobForm';
+import {
+	HydrationBoundary,
+	QueryClient,
+	dehydrate,
+} from '@tanstack/react-query';
 
 export default function AddJobsPage() {
+	const queryClient = new QueryClient();
+
 	return (
-		<>
+		<HydrationBoundary state={dehydrate(queryClient)}>
 			<CreateJobForm />
-		</>
+		</HydrationBoundary>
 	);
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,24 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
+import { useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: { queries: { staleTime: 60 * 1000 * 5 } },
+			})
+	);
 	return (
 		<>
-			<ThemeProvider
-				attribute='class'
-				defaultTheme='system'
-				enableSystem
-			>
-				{children}
+			<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+				<Toaster />
+				<QueryClientProvider client={queryClient}>
+					{children}
+				</QueryClientProvider>
 			</ThemeProvider>
 		</>
 	);
