0b6fedb0f0a38d2e80784d6d9caa7ecbb141cc12
jackal7819 <jackal3166@gmail.com>
2024-05-03 11:30:47 +0300

    Add search

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -10,22 +10,39 @@ import {
 	SelectValue,
 } from './ui/select';
 import { Button } from './ui/button';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function SearchForm() {
+	const router = useRouter();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	const search = searchParams.get('search') || '';
+	const jobStatus = searchParams.get('jobStatus') || 'all';
+
 	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 		const formData = new FormData(e.currentTarget);
 		const search = formData.get('search') as string;
 		const jobStatus = formData.get('jobStatus') as string;
-		console.log(search, jobStatus);
+		let params = new URLSearchParams();
+		params.set('search', search);
+		params.set('jobStatus', jobStatus);
+
+		router.push(`${pathname}?${params.toString()}`);
 	}
 	return (
 		<form
 			className='grid gap-4 p-8 mb-16 rounded-lg bg-muted sm:grid-cols-2 md:grid-cols-3'
 			onSubmit={handleSubmit}
 		>
-			<Input name='search' type='text' placeholder='Search Jobs' />
-			<Select name='jobStatus'>
+			<Input
+				name='search'
+				type='text'
+				placeholder='Search Jobs'
+				defaultValue={search}
+			/>
+			<Select name='jobStatus' defaultValue={jobStatus}>
 				<SelectTrigger>
 					<SelectValue />
 				</SelectTrigger>
