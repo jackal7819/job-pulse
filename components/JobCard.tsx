602ade7699531044860b956b0b546ee8fea62c27
jackal7819 <jackal3166@gmail.com>
2024-05-03 22:03:30 +0300

    Add JobCard

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,13 +1,37 @@
 import { JobType } from '@/utils/types';
 
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from './ui/card';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import DeleteJobBtn from './DeleteJobBtn';
+
 interface JobCardProps {
 	job: JobType;
 }
 
 export default function JobCard({ job }: JobCardProps) {
 	return (
-		<div>
-			<h1>JobCard</h1>
-		</div>
+		<Card className='bg-muted'>
+			<CardHeader>
+				<CardTitle>{job.position}</CardTitle>
+				<CardDescription>{job.company}</CardDescription>
+			</CardHeader>
+			<Separator />
+			<CardContent></CardContent>
+			<CardFooter className='flex gap-4'>
+				<Button asChild size='sm'>
+					<Link href={`/jobs/${job.id}`}>Edit</Link>
+				</Button>
+				<DeleteJobBtn />
+			</CardFooter>
+		</Card>
 	);
 }
