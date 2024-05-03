bcc9874589bed88ccfbc3596c9c479920fb469e1
jackal7819 <jackal3166@gmail.com>
2024-05-03 16:00:53 +0300

    Add jobs.map

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,4 +1,10 @@
-export default function JobCard() {
+import { JobType } from '@/utils/types';
+
+interface JobCardProps {
+	job: JobType;
+}
+
+export default function JobCard({ job }: JobCardProps) {
 	return (
 		<div>
 			<h1>JobCard</h1>
--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -1,7 +1,46 @@
-export default function JobsList() {
+'use client';
+
+import { getAllJobsAction } from '@/utils/actions';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
+import JobCard from './JobCard';
+
+export default function JobList() {
+	const searchParams = useSearchParams();
+
+	const search = searchParams.get('search') || '';
+	const jobStatus = searchParams.get('jobStatus') || 'all';
+	const pageNumber = Number(searchParams.get('page')) || 1;
+
+	const { data, isPending } = useQuery({
+		queryKey: ['jobs', search, jobStatus, pageNumber],
+		queryFn: () =>
+			getAllJobsAction({ search, jobStatus, page: pageNumber }),
+	});
+
+	const jobs = data?.jobs || [];
+
+	if (isPending) {
+		return (
+			<div>
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
+
+	if (jobs.length < 1) {
+		return (
+			<div>
+				<h1>No Jobs Found...</h1>
+			</div>
+		);
+	}
+
 	return (
-		<div>
-			<h1>JobsList</h1>
-		</div>
+		<>
+			<div className='grid gap-8 md:grid-cols-2'>
+				{jobs.map((job) => <JobCard key={job.id} job={job} />)}
+			</div>
+		</>
 	);
 }
